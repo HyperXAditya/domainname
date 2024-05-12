02931bdd8cab859825d68fd5e4ef56df90af564b
HyperXAditya <adityarajofficial834@gmail.com>
2024-05-12 21:04:43 +0530

    1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+ReactDOM.createRoot(<React.StrictMode >
+	<App />
+</React.StrictMode>,
+	document.getElementById('root')
+);
